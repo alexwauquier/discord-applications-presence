73d358ef517e8dfff445f225b57f897d31785c2c
Alex Wauquier <alex@alexwauquier.com>
2025-10-12 22:29:56 +0200

    chore: declare global interface for window.discordAPI

--- src/preload.ts
+++ src/preload.ts
@@ -3,6 +3,12 @@
 
 import { contextBridge, ipcRenderer } from 'electron';
 
+declare global {
+  interface Window {
+    discordAPI: any;
+  }
+}
+
 contextBridge.exposeInMainWorld('discordAPI', {
   getDetectableApplications: () => ipcRenderer.invoke('get-discord-detectable-applications')
 });
